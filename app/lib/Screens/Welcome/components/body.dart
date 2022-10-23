8d5c709139b100db3e31af18fdaebab4a3e967bf
JoãoRenato <joaorenatovaz@gmail.com>
2022-10-22 21:47:24 -0300

    mudando a imagen

--- app/lib/Screens/Welcome/components/body.dart
+++ app/lib/Screens/Welcome/components/body.dart
@@ -3,23 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     // Pegando o tamanho e a largura da tela
     Size size = MediaQuery.of(context).size;
-    return Background(child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Bem Vindo!",
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Bem Vindo!",
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          SvgPicture.asset("assets/icons/Thesis-pana.svg", height: size.height * 0.4,)
-          ],
-    ),
+          SvgPicture.asset(
+            "assets/icons/chat.svg",
+            height: size.height * 0.45,
+          )
+        ],
+      ),
     );
   }
 }
-
-
